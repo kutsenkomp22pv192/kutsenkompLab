53fa01dc46f0fd1746f84291101ec53d25c4e8fc
kutsenkomp22pv192 <imaria.ku@icloud.com>
2022-11-23 22:10:23 +0300

    little fix

--- src/main/java/tech/reliab/course/kutsenkomp/bank/Main.java
+++ src/main/java/tech/reliab/course/kutsenkomp/bank/Main.java
@@ -18,8 +18,7 @@ public class Main {
         CreditAccountService creditAccountService = CreditAccountServiceImpl.getInstance();
         PaymentAccountService paymentAccountService = PaymentAccountServiceImpl.getInstance();
         UserService userService = UserServiceImpl.getInstance();
-        int idEmployee = 0;
-        int idAccount = 0;
+
         DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
         final int banksNumber = 5;
@@ -28,32 +27,30 @@ public class Main {
         final int usersNumber = 5;
         final int accountsNumber = 2;
 
-
         for (int i = 0; i < banksNumber; i++) {
             int idBank = i;
             var bank = bankService.add(new Bank(idBank, "Bank_" + idBank, 0, 0, 0, 0,1, 0, 1 ));
             Employee employeeForAccount = null;
             for (int j = 0; j < officesNumber; j++) {
-                int idBankOffice = i * banksNumber + j;
+                int idBankOffice = i * officesNumber + j;
                 var bankOffice = bankOfficeService.add(new BankOffice(bank, idBankOffice, "Office_" + idBankOffice, "Address_" + idBankOffice,"Working", true,  true,true, 0, true, 1, 1));
                 Employee employee = null;
                 for (int k = 0; k < employeeNumber; k++) {
+                    int idEmployee = i * officesNumber * employeeNumber + j * employeeNumber + k;
                     employee = employeeService.add(new Employee(idEmployee, "FullName_" + idEmployee, sdf.parse("1988-02-" + idEmployee), "Pos_" + idEmployee, bank, true, bankOffice, true, 1));
-                    idEmployee+=1;
                     if (employeeForAccount == null) {
                         employeeForAccount = employee;
-
                     }
                 }
                 atmService.add(new BankAtm(idBankOffice, "Atm_" + idBankOffice, "Address_" + idBankOffice, "Working", bank, bankOffice, employee, true,  true,1, 1));
             }
             for (int j = 0; j < usersNumber; j++) {
-                int idUser = i * banksNumber + j;
+                int idUser = i * usersNumber + j;
                 var user = userService.add(new User(idUser, "fullName_" + idUser, sdf.parse("1999-01-" + idUser),  "Address_" + idUser, 1, bank, 1));
                 for (int k = 0; k < accountsNumber; k++) {
+                    int idAccount = i * usersNumber * accountsNumber + j * accountsNumber + k;
                     var paymentAccount = paymentAccountService.add(new PaymentAccount(idAccount, user, "Bank_" + i, 1));
                     var creditAccount = creditAccountService.add(new CreditAccount(idAccount, user, "Bank_" + i, 1, new Date(), 1, 1, 1, employeeForAccount, paymentAccount));
-                    idAccount+=1;
                 }
             }
         }
