0164bee0887483d83cc06968c3087b3357f74019
kutsenkomp22pv192 <imaria.ku@icloud.com>
2022-11-27 01:04:24 +0300

    lab3

--- src/main/java/tech/reliab/course/kutsenkomp/bank/comparators/BankComparator.java
+++ src/main/java/tech/reliab/course/kutsenkomp/bank/comparators/BankComparator.java
@@ -8,30 +8,30 @@ public class BankComparator implements Comparator<Bank> {
     @Override
     public int compare(Bank left, Bank right) {
 
-//        var interestRateCompare = Float.compare(left.getInterestRate(),right.getInterestRate());
-//        if(interestRateCompare != 0){
-//            return -interestRateCompare;
-//        }
+        var interestRateCompare = Float.compare(left.getInterestRate(),right.getInterestRate());
+        if(interestRateCompare != 0){
+            return -interestRateCompare;
+        }
 
         if(left.getRate() != right.getRate()){
             return left.getRate() < right.getRate() ? 1: -1;
         }
-//
-//        if(left.getCountClients() != right.getCountClients()){
-//            return left.getCountClients() > right.getCountClients() ? 1: -1;
-//        }
-//
-//        if(left.getCountOffices() != right.getCountOffices()){
-//            return left.getCountOffices() > right.getCountOffices() ? 1: -1;
-//        }
-//
-//        if(left.getCountAtm() != right.getCountAtm()){
-//            return left.getCountAtm() > right.getCountAtm() ? 1: -1;
-//        }
-//
-//        if(left.getCountEmployees() != right.getCountEmployees()){
-//            return left.getCountEmployees() > right.getCountEmployees() ? 1: -1;
-//        }
+
+        if(left.getCountClients() != right.getCountClients()){
+            return left.getCountClients() > right.getCountClients() ? 1: -1;
+        }
+
+        if(left.getCountOffices() != right.getCountOffices()){
+            return left.getCountOffices() > right.getCountOffices() ? 1: -1;
+        }
+
+        if(left.getCountAtm() != right.getCountAtm()){
+            return left.getCountAtm() > right.getCountAtm() ? 1: -1;
+        }
+
+        if(left.getCountEmployees() != right.getCountEmployees()){
+            return left.getCountEmployees() > right.getCountEmployees() ? 1: -1;
+        }
 
         return 0;
     }
